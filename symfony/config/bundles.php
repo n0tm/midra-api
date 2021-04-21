5ebead3aaddec128e4ed97046a6a4e3aff60c53c
andrey.kutin <andrey.kutin@cauri.com>
2021-04-21 21:36:29 +0300

    Update receipts

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -11,4 +11,6 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
